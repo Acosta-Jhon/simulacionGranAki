5acdc4bd601352799d7c07d96f91b346f0fa3f09
Acosta-Jhon <jhonerick2018@gmail.com>
2020-06-26 21:02:03 -0500

    Primer Commit

---
+++ Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace comprandoEnAki
+{
+    class Persona
+    {
+        //Propiedades
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Cedula { get; set; }
+        public string Direccion { get; set; }
+
+        //Metodos
+        public string Saludar(string nombreSucursal)
+        {
+            return $"Bienvenido {this.Nombre} a {nombreSucursal}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace comprandoEnAki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random codigoLocal = new Random();
+            int id = codigoLocal.Next();
+            string nombreSucursal = "Gran Aki";
+            string direccionSucursal = "Av. Colon ";
+
+            //Instancias
+            Persona cajero = new Persona
+            {
+                Id = new Random().Next(),
+                Nombre = "Cristal",
+                Cedula = "1789456123",
+                Direccion = "Sector la Comunda"
+            };
+
+            Persona cliente = new Persona
+            {
+                Id = new Random().Next(),
+                Nombre = "Jhon",
+                Cedula = "17445698725",
+                Direccion = "Sector San Juan"
+            };
+
+            //Consolas
+            string primerCliente = cliente.Nombre;
+            string saludo = cajero.Saludar(primerCliente);
+            Console.WriteLine(saludo);
+            Console.ReadKey();
+        }
+
+    }
+}
